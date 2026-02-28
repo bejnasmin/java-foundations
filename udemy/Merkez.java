fa862f0795c2f1d20aaf07b210e4d19c0fed9768
Bejna Asmin Örçen <bejnaasmin@Bejnas-MacBook-Pro.local>
2026-02-28 20:29:39 +0300

    udemy file tasks

---
+++ udemy/Merkez.java
@@ -0,0 +1,15 @@
+package udemy;
+
+
+public class Merkez {
+
+	public static void main(String[] args) {
+	    
+	System.out.println("Hello World");
+	
+	//burada ben isim ve soysim yazdirmasi yaptim .
+	System.out.println("Bejna");
+	//kod
+	}
+
+}
